a44d9bd6f57cf68552125a6d0c96d45fd14fcfb8
Anthony <anthonyzhu132@gmail.com>
2019-09-11 01:16:29 +0000

    created new tweet function that appends after creation

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,3 +4,48 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+const tweetData = {
+  "user": {
+    "name": "Newton",
+    "avatars": "https://i.imgur.com/73hZDYK.png",
+    "handle": "@SirIsaac"
+  },
+  "content": {
+    "text": "If I have seen further it is by standing on the shoulders of giants"
+  },
+  "created_at": 1461116232227
+};
+
+const createTweetElement = (data) => {
+  const name = data.user.name;
+  const avatars = data.user.avatars;
+  const handle = data.user.handle;
+  const content = data.content.text;
+  const createdAt = data.created_at;
+
+  return `
+  <article class="tweet">
+  <header>
+    <img id="avatar" src="${avatars}">
+    <h3>${name}</h3>
+    <h4>${handle}</h4>
+  </header>
+  <p>${content}</p>
+  <footer>
+    <span class="date">${createdAt}</span>
+  </footer>
+</article>
+  `;
+};
+
+const tweet = createTweetElement(tweetData);
+
+$(document).ready(function() {
+  $('.tweets-container').append(tweet);
+  $('.tweets-container').append(tweet);
+  $('.tweets-container').append(tweet);
+  $('.tweets-container').append(tweet);
+  $('.tweets-container').append(tweet);
+  $('.tweets-container').append(tweet);
+  
+});
