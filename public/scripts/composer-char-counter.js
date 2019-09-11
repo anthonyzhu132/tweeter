e197015cff7e8c69d6c7ca91e90a70df290824c0
Anthony <anthonyzhu132@gmail.com>
2019-09-11 00:24:18 +0000

    initial commit for all of my files for tweeter app

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,16 @@
+console.log("test outside of document ready");
+
+const maxChars = 140;
+
+$(document).ready(function() {
+  $('.new-tweet textarea').on('input', function() {
+    const characters = $(this).val().length;
+    const counter = maxChars - characters;
+    if (counter < 0) {
+      $('.counter').css('color', 'red');
+    } else {
+      $('.counter').css('color', 'black');
+    }
+    $(".counter").text(counter);
+  });
+});
