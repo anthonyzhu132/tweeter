b8e58359cef7fb153bf931d341f0a92f855214f3
Anthony <anthonyzhu132@gmail.com>
2019-09-11 01:37:30 +0000

    created renderTweets function to convert array of data into createTweetElement function

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,16 +4,35 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-const tweetData = {
-  "user": {
-    "name": "Newton",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
-    "handle": "@SirIsaac"
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
   },
-  "content": {
-    "text": "If I have seen further it is by standing on the shoulders of giants"
-  },
-  "created_at": 1461116232227
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+];
+
+const renderTweets = (tweetArray) => {
+  tweetArray.forEach(data => {
+    $('.tweets-container').append(createTweetElement(data));
+  });
 };
 
 const createTweetElement = (data) => {
@@ -21,7 +40,7 @@ const createTweetElement = (data) => {
   const avatars = data.user.avatars;
   const handle = data.user.handle;
   const content = data.content.text;
-  const createdAt = data.created_at;
+  const createdAt = Date.now();
 
   return `
   <article class="tweet">
@@ -34,18 +53,13 @@ const createTweetElement = (data) => {
   <footer>
     <span class="date">${createdAt}</span>
   </footer>
-</article>
+  </article>
   `;
 };
 
-const tweet = createTweetElement(tweetData);
+
+
 
 $(document).ready(function() {
-  $('.tweets-container').append(tweet);
-  $('.tweets-container').append(tweet);
-  $('.tweets-container').append(tweet);
-  $('.tweets-container').append(tweet);
-  $('.tweets-container').append(tweet);
-  $('.tweets-container').append(tweet);
-  
+  renderTweets(data);
 });
